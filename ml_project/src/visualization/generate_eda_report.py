4d516b956e1c9cefc2bd22227150489a79b870c0
sergeylizanmen <lizanmensergej@gmail.com>
2022-10-29 22:29:51 +0300

    eda add, generate eda add

---
+++ ml_project/src/visualization/generate_eda_report.py
@@ -0,0 +1,20 @@
+import click
+import pandas as pd
+from dataprep.eda import create_report
+
+
+@click.command()
+@click.option('--path_to_data',
+              default='data/raw/heart_cleveland_upload.csv',
+              type=click.Path(exists=True))
+@click.option('--path_to_save',
+              default='report/eda.html',
+              type=click.Path(exists=False))
+def generate_eda_report(path_to_data, path_to_save):
+    df = pd.read_csv(path_to_data)
+    report = create_report(df)
+    report.save(path_to_save)
+
+
+if __name__ == '__main__':
+    generate_eda_report()
