9507c229e989107a8896a06b6a79691135d40135
sergeylizanmen <lizanmensergej@gmail.com>
2022-10-30 19:25:50 +0300

    add logging

---
+++ ml_project/src/settings/__init__.py
@@ -0,0 +1,3 @@
+from .logging_conf import get_logging_conf
+
+__all__ = ['get_logging_conf']
--- ml_project/src/train_pipeline.py
+++ ml_project/src/train_pipeline.py
@@ -1,25 +1,36 @@
 import json
 import click
 
+import logging.config
+from src.settings import get_logging_conf
+
 from src.data import read_data, split_train_val_data
 from src.features import extract_target, build_transformer
 from src.models import evaluate_model, train_model, predict_model, dump_model
 from src.enities import read_train_pipeline_params, TrainingPipelineParams
 
+logging.config.dictConfig(get_logging_conf())
+logger = logging.getLogger(__name__)
+
 
 def train_pipeline(
         train_pipeline_params: TrainingPipelineParams
 ) -> tuple[str, dict[str, float]]:
+    logger.info(f"start train pipeline with params {train_pipeline_params}")
     data = read_data(train_pipeline_params.path_to_data)
+    logger.info(f"data.shape is {data.shape}")
 
     train_df, val_df = split_train_val_data(
         data, train_pipeline_params.splitting_params
     )
     train_df, train_target = extract_target(train_df, train_pipeline_params.feature_params)
     val_df, val_target = extract_target(val_df, train_pipeline_params.feature_params)
+    logger.info(f"train_df.shape is {train_df.shape}")
+    logger.info(f"val_df.shape is {val_df.shape}")
 
     transformer = build_transformer(train_pipeline_params.feature_params)
     train_features = transformer.fit_transform(train_df)
+    logger.info(f"train_features.shape is {train_features.shape}")
 
     model = train_model(train_features, train_target, train_pipeline_params.train_params)
 
@@ -27,11 +38,14 @@ def train_pipeline(
     predicts = predict_model(val_features, model)
 
     metrics = evaluate_model(predicts, val_target)
+    logger.info(f"metrics is {metrics}")
 
     with open(train_pipeline_params.path_to_metrics, "w") as f:
         json.dump(metrics, f)
+        logger.info(f"metrics saved by {train_pipeline_params.path_to_metrics}")
 
     path_to_model = dump_model(model, train_pipeline_params.path_to_output)
+    logger.info(f"model saved by {train_pipeline_params.path_to_output}")
     return path_to_model, metrics
 
 
